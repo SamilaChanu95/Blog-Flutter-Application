d474726cd54646b6a14a372c0a4f7515a6504af3
SamilaChanu95 <chanukasamila@gmail.com>
2019-10-12 21:17:19 +0530

    Add the SignIn, SignUp, getCurrentuser, signOut methods for AuthImplementation abstract class.

--- lib/Authentication.dart
+++ lib/Authentication.dart
@@ -1,34 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
  
- abstract class AuthImplemenatation
- {Future<void> signOut() async
-    {
-      _firebaseAuth.signOut();
-    }
-
+ abstract class AuthImplementation
+ {
+   Future<String> SignIn(String email, String password);
+   Future<String> SignUp(String email, String password);
+   Future<String> getCurrentuser();
+   Future<void> signOut();
  }
 
 
- class Auth implements AuthImplemenatation
+ class Auth implements AuthImplementation
  {
+   //private variable
    final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
 
     //asynchronization method for SignIn
     Future<String> SignIn(String email, String password) async
    {
-     FirebaseUser user = await _firebaseAuth.signInWithEmailAndPassword(email: email, password: password);
+     FirebaseUser user =  (await _firebaseAuth.signInWithEmailAndPassword(email: email, password: password)).user;
      return user.uid;
-   }
+   } 
 
-    //asynchronization method for SignUp
+      //asynchronization method for SignUp
     Future<String> SignUp(String email, String password) async
    {
-     FirebaseUser user = await _firebaseAuth.signInWithEmailAndPassword(email: email, password: password);
+     FirebaseUser user = (await _firebaseAuth.createUserWithEmailAndPassword(email: email, password: password)).user;
      return user.uid;
    }
+  
 
-    //get the userid whho goto the login
+    //get the userid who goto the Login to the system
     Future<String> getCurrentuser() async
     {
       FirebaseUser user = await _firebaseAuth.currentUser();
