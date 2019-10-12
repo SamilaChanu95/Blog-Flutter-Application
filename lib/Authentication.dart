aed06dd445ff482b3015b7dceaf28226ec496a60
SamilaChanu95 <chanukasamila@gmail.com>
2019-10-12 12:37:27 +0530

    Create authentication asynchronization methods also SignIn, SignOut, SignUp and getCurrentuser methods.

--- lib/Authentication.dart
+++ lib/Authentication.dart
@@ -2,7 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
  
  abstract class AuthImplemenatation
- {
+ {Future<void> signOut() async
+    {
+      _firebaseAuth.signOut();
+    }
 
  }
 
@@ -11,27 +14,31 @@ import 'package:firebase_auth/firebase_auth.dart';
  {
    final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
 
+    //asynchronization method for SignIn
     Future<String> SignIn(String email, String password) async
    {
      FirebaseUser user = await _firebaseAuth.signInWithEmailAndPassword(email: email, password: password);
      return user.uid;
    }
 
+    //asynchronization method for SignUp
     Future<String> SignUp(String email, String password) async
    {
      FirebaseUser user = await _firebaseAuth.signInWithEmailAndPassword(email: email, password: password);
      return user.uid;
    }
 
+    //get the userid whho goto the login
     Future<String> getCurrentuser() async
     {
       FirebaseUser user = await _firebaseAuth.currentUser();
       return user.uid; 
     }
     
+    //asynchronization method for SignOut
     Future<void> signOut() async
     {
-      _firebaseAuth.signOut();
+      _firebaseAuth.signOut();//using the firebase authentication
     }
 
  }
