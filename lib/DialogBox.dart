cca36431f2cd5dbe10b77be6a63d96b4207be7c3
SamilaChanu95 <chanukasamila@gmail.com>
2019-10-12 21:48:42 +0530

    create dialogbox class.

---
+++ lib/DialogBox.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+
+class DialogBox
+{
+  information(BuildContext context, String title, String description)
+  {
+    return showDialog
+    (
+      context: context,
+      barrierDismissible: true,
+
+      builder: (BuildContext context)
+      {
+        return AlertDialog
+        (
+          title: Text(title),
+          content: SingleChildScrollView
+          (
+            child: ListBody
+            (
+              children: <Widget>
+              [
+                Text(description)
+              ],
+
+            ),
+
+          ),
+
+          actions: <Widget>
+          [
+            FlatButton
+            (
+              child: Text("ok"),
+
+              onPressed: ()
+              {
+                return Navigator.pop(context);
+              },
+            )
+          ],
+
+        );
+      }
+    );
+  }
+}
